58a8c6695929e1a0087d23f39844a2525f70805f
aidabo <aidabo@janabiz.com>
2025-05-21 00:32:20 +0900

    remove old migration js

--- ghost/core/core/server/data/migrations/versions/5.115/2025-04-01-18-00-00-add-group-column-to-post.js
+++
@@ -1,9 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('posts', 'group_id', {
-    type: 'string',
-    maxlength: 24,
-    nullable: true,
-    references: 'social_groups.id',
-    cascadeDelete: true
-});
