a9074d68eec9ead53cc5eca0dce609214a49eae6
aidabo <aidabo@janabiz.com>
2025-06-17 01:01:25 +0900

    docker image build script changed

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,12 @@ VERSION="5.116.2-alpine-next"
 # yarn docker:build
 yarn docker:next:build
 
+# Tag for Docker Hub
+docker tag $IMAGE_NAME:$VERSION jbcdev99ai/$IMAGE_NAME:$VERSION
+
+# Push to Docker Hub
+docker push jbcdev99ai/$IMAGE_NAME:$VERSION
+
 # Save image as .tar
 docker save -o $IMAGE_NAME-$VERSION.tar $IMAGE_NAME:$VERSION
 
@@ -20,3 +26,4 @@ aws s3 cp $IMAGE_NAME-$VERSION.tar s3://$S3_BUCKET/$S3_FOLDER/$IMAGE_NAME-$VERSI
 rm $IMAGE_NAME-$VERSION.tar
 
 echo "Upload completed: s3://$S3_BUCKET/$S3_FOLDER/$IMAGE_NAME-$VERSION.tar"
+
